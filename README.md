6efa5568041da3dd10f4cf7f7e1c963c246b2e39
abonnin37 <bonnin.a.k@gmail.com>
2021-11-10 07:52:13 -0300

    Static tab exemple

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Heatmap chart in vanilla js
 
 This chart is created to be shared to persons that may need it.
+
+Source :
+[1] https://www.w3.org/WAI/tutorials/tables/irregular/
---
+++ heatmap.js
@@ -0,0 +1,27 @@
+class HeatMap {
+    constructor(container, config) {
+        this.container = container;
+        this.config = config;
+    }
+
+   /** Structure du paramètre config
+    * 
+    * config: {
+    *   header: {
+    *       group_number: number,
+    *       backgroundColor: string,
+    *       data: [
+    *           {
+    *               title: string,
+    *               group_column_number: number,
+    *               column_names: string[]
+    *           },
+    *           ...
+    *       ]
+    *   },
+    *   data: {
+    * 
+    *   }
+    * }
+    */
+}
